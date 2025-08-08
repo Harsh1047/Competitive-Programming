ffa56f8d98baf82ae13fb113b16bcfcfbf034f4a
Harsh1047 <jainharsh1047@gmail.com>
2025-08-08 10:10:40 +0530

    Solved: Main

---
+++ Project1/src/Main.java
@@ -0,0 +1,42 @@
+public class Main{
+    public static void main(String[] args) {
+//        float [] marks = {45.4f, 36.0f, 65.4f, 87.6f};
+//        float sum = 0;
+//        for(float elements: marks){
+//            sum = sum + elements;
+//        }
+//        System.out.println(sum);
+//        int[] marks = {61,65,87,21,67,62,78};
+//        int num = 62;
+//        for(int i=0;i<marks.length;i++){
+//            if(marks[i] == num){
+//                System.out.println("Same no. - " + num);
+//                break;
+//            }
+//        }
+//        int[] arr = {1,2,3,4,5};
+//        for(int i=1;i<(arr.length/2)+1;i++){
+//            arr[i] = arr[arr.length-1-i];
+//            System.out.println(arr[i]);
+//        }
+//        fab(3);
+//        table(5);
+
+    }
+//    public static void fab(int n){
+//        int n1=0,n2=1;
+//        for(int i = 0; i <=n ; i++){
+//            System.out.println(n1);
+//            int n3= n1+n2;
+//            n1=n2;
+//            n2=n3;
+//          }
+//       }
+
+//    static void table(int n){
+//        for(int i = 0; i<=10;i++){
+//            System.out.println(n + "*" + i + "=" + n*i);
+//        }
+//    }
+
+}
