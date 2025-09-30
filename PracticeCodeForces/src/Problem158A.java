61dc87461bf16f5e96b11c683f93e73701c1a93a
Harsh1047 <jainharsh1047@gmail.com>
2025-09-30 08:14:46 +0530

    Solved: Problem158A

---
+++ PracticeCodeForces/src/Problem158A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem158A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>=arr[k-1] && arr[i]>0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
