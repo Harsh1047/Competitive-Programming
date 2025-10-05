4bf9001e8bd0a6734ad57162511be3959b52619e
Harsh1047 <jainharsh1047@gmail.com>
2025-10-05 16:05:41 +0530

    Solved: Sorting_Bubblesort

---
+++ PracticeCodeForces/src/Sorting_Bubblesort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Sorting_Bubblesort {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            int k = sc.nextInt();
+            arr[i] = k;
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
