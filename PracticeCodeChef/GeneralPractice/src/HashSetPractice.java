6ebd969b2dd5fa43af402c5e8b29c285e2ffb598
Harsh1047 <jainharsh1047@gmail.com>
2025-10-12 12:15:46 +0530

    Solved: HashSetPractice

---
+++ PracticeCodeChef/GeneralPractice/src/HashSetPractice.java
@@ -0,0 +1,8 @@
+import java.util.HashSet;
+import java.util.Iterator;
+public class HashSetPractice {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+    }
+}
