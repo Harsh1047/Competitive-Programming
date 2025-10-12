270f2abf0ce1f4d91fc1a67e298116713f0d29f9
Harsh1047 <jainharsh1047@gmail.com>
2025-10-12 11:19:54 +0530

    Solved: Hashing2

---
+++ PracticeCodeForces/src/Hashing2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Hashing2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int q = sc.nextInt();
+        int[] hash = new int[26];
+        for(int i=0;i<s.length();i++){
+            hash[s.charAt(i) - 'a']++;
+        }
+        while(q-->0){
+            char c = sc.next().charAt(0);
+            System.out.println(hash[c-'a']);
+        }
+    }
+}
