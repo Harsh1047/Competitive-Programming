15b354339ba3e1dd3cf487c207b6a36b33c6d257
Harsh1047 <jainharsh1047@gmail.com>
2025-10-22 20:00:49 +0530

    Solved: ProblemP1209

---
+++ PracticeCodeChef/src/ProblemP1209.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ProblemP1209 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<=4){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
