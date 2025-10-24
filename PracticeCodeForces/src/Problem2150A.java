4f0a9c2838e9f6d5a18441ee0b28332a037fc480
Harsh1047 <jainharsh1047@gmail.com>
2025-10-24 07:55:48 +0530

    Solved: Problem2150A

---
+++ PracticeCodeForces/src/Problem2150A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem2150A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            String str = sc.next();
+            int[] arr = new int[m];
+            for(int i=0;i<m;i++){
+                arr[i] = sc.nextInt();
+            }
+        }
+    }
+}
