ab564d8bf7fc584990e8546b3e5794055847286c
Harsh1047 <jainharsh1047@gmail.com>
2025-11-08 16:05:33 +0530

    Solved: Problem208A

---
+++ PracticeCodeForces/src/Problem208A.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Problem208A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        StringBuilder sb = new StringBuilder(str);
+        for(int i=0;i<sb.length();i++){
+        }
+    }
+}
