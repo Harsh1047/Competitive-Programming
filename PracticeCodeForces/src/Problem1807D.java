8fb8a49d688327798e51ab522e116bc313256d49
Harsh1047 <jainharsh1047@gmail.com>
2025-11-11 14:55:49 +0530

    Solved: Problem1807D

---
+++ PracticeCodeForces/src/Problem1807D.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Problem1807D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int sum=0;
+            int n = sc.nextInt();
+            int q = sc.nextInt();
+            int[] arr = new int[n];
+            ArrayList<Integer> arl = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                arr[i] = sc.nextInt();
+                arl.add(arr[i]);
+            }
+            for(int i=0;i<=q;i++){
+                int l = sc.nextInt();
+                int r = sc.nextInt();
+                int k = sc.nextInt();
+
+            }
+
+
+        }
+    }
+}
