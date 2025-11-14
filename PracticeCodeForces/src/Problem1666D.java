154e589796ffc5430542e2c7bc718feb24aebdfb
Harsh1047 <jainharsh1047@gmail.com>
2025-11-14 09:34:07 +0530

    Solved: Problem1666D

---
+++ PracticeCodeForces/src/Problem1666D.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Problem1666D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            String str = sc.next();
+            StringBuilder sb = new StringBuilder(str);
+            String word = sc.next();
+            StringBuilder sb2 = new StringBuilder(str);
+
+        }
+    }
+}
