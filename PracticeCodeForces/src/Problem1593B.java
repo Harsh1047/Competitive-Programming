a53c24a9cb1e61a4f17a147103710e0adacde464
Harsh1047 <jainharsh1047@gmail.com>
2025-11-24 14:29:05 +0530

    Solved: Problem1593B

---
+++ PracticeCodeForces/src/Problem1593B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Problem1593B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            String n = sc.next();
+        }
+    }
+}
