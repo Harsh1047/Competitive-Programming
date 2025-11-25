0a067890d4c2c9cf4b1b3d35abdc898f8d78ee42
Harsh1047 <jainharsh1047@gmail.com>
2025-11-25 14:36:32 +0530

    Solved: Problem1954ANotSolved

---
+++ PracticeCodeForces/src/Problem1954ANotSolved.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem1954ANotSolved {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            boolean istrue = false;
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int k = sc.nextInt();
+            if(m<=k){
+                System.out.println("NO");
+            }
+            else{
+                System.out.println("YES");
+            }
+        }
+    }
+}
