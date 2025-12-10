576f89b670b61a6b46df7666fe67f34bd8cbd58f
Harsh1047 <jainharsh1047@gmail.com>
2025-12-10 21:48:47 +0530

    Solved: Pyramid

---
+++ PracticeCodeChef/GeneralPractice/src/Pyramid.java
@@ -0,0 +1,31 @@
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class Pyramid {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+////        for (int i=1;i<=5;i++){
+////            for(int j=i;j<5-i;j++){
+////                System.out.print(" ");
+////            }
+////            for(int j=0;j<2*i-1;j++){
+////                System.out.print("*");
+////            }
+//////            for(int j=i;j<9/2-i;j++){
+//////                System.out.print(" ");
+//////            }
+////            System.out.println();
+////        }
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        for(int i=0;i<n;i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        int hash[] = new int[n+1];
+//        for(int i=0;i<n;i++) {
+//            hash[arr[i]]++;
+//        }
+//        Arrays.sort(hash);
+//        System.out.println(hash[0]);
+//    }
+//}
