c6d2bc25fdcb0b1f6225530e42aeb61663a62b33
Harsh1047 <jainharsh1047@gmail.com>
2025-12-15 18:48:28 +0530

    Solved: ProblemENTERTAIN

---
+++ PracticeCodeChef/src/ProblemENTERTAIN.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ProblemENTERTAIN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n < 5) {
+            System.out.println(n*200);
+        }
+        else{
+            System.out.println(1000);
+        }
+    }
+}
