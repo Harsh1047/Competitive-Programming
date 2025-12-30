cfb14fefa33c348e88fbc68303413d6e906d4616
Harsh1047 <jainharsh1047@gmail.com>
2025-12-30 09:20:00 +0530

    Solved: ProblemBUY1GET1

---
+++ PracticeCodeChef/src/ProblemBUY1GET1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ProblemBUY1GET1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            String str = sc.next();
+            for(int i=0;i<str.length();i++){
+            }
+        }
+    }
+}
