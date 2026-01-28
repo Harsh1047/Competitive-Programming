3df2b35abcd14bac383d893b737880ab56008c5e
Harsh1047 <jainharsh1047@gmail.com>
2026-01-28 19:57:13 +0530

    Solved: Problem223D_02

---
+++ PracticeCodeChef/src/Problem223D_02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem223D_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            System.out.println((Math.min(x,z) + y/2));
+        }
+    }
+}
