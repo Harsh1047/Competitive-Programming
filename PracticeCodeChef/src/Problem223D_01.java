fb34b50153eff48a2ac902ee78d9e401450a6306
Harsh1047 <jainharsh1047@gmail.com>
2026-01-28 19:57:04 +0530

    Solved: Problem223D_01

---
+++ PracticeCodeChef/src/Problem223D_01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Problem223D_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if(n>=200) System.out.println(n-50);
+            else System.out.println(n);
+    }
+}
