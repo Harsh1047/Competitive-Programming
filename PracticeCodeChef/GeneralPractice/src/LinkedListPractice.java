898b31fa057616a7b4281f5802641bc7fc405b37
Harsh1047 <jainharsh1047@gmail.com>
2026-02-04 14:39:12 +0530

    Solved: LinkedListPractice

---
+++ PracticeCodeChef/GeneralPractice/src/LinkedListPractice.java
@@ -0,0 +1,25 @@
+public class LinkedListPractice {
+    static class Node{
+        int data;
+        Node node;
+        Node(int data1, Node node1){
+            this.data = data1;
+            this.node = node1;
+        }
+        Node(int data1){
+            this.data = data1;
+            this.node = null;
+        }
+    }
+    static Node conv(int[] arr, Node head){
+        Node mover = head;
+        for(int i=0;i<arr.length;i++){
+            Node temp = head;
+
+        }
+        return mover;
+    }
+    public static void main(String[] args) {
+
+    }
+}
