3ff4567279f5822e618419df9ce3082ccebe47ef
Harsh1047 <jainharsh1047@gmail.com>
2026-02-23 13:34:27 +0530

    Solved: ProblemRECTANGL

---
+++ PracticeCodeChef/src/ProblemRECTANGL.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ProblemRECTANGL {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+            if(a==b && c==d || a==c && b==d || a==d && b==c) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
