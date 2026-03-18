4e124ae9e46069b0a5d70d03d4cfdf4a3f67c3a8
Harsh1047 <jainharsh1047@gmail.com>
2026-03-18 14:17:28 +0530

    Solved: ProblemMINPIZZAS

---
+++ PracticeCodeChef/src/ProblemMINPIZZAS.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ProblemMINPIZZAS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+//            while(true){
+//                if(n<k) n++;
+//                else if(n%k==0) {
+//                    s=n/k;
+//                    break;
+//                }
+//                n++;
+//            }
+//            System.out.println(s);
+        }
+    }
+}
