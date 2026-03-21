6e8e144e01abf13a3dedd78fa3519d52a00e240f
Harsh1047 <jainharsh1047@gmail.com>
2026-03-21 17:33:18 +0530

    Solved: Problem723A

---
+++ PracticeCodeForces/src/Problem723A.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem723A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        for(int i=0;i<3;i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        System.out.println(Math.abs(arr[1]- arr[0]) + Math.abs(arr[2]-arr[1]));
+    }
+}
