612951bb499da467164310e052dca808f9f756e6
Harsh1047 <jainharsh1047@gmail.com>
2026-03-22 10:35:42 +0530

    Solved: Problem785A

---
+++ PracticeCodeForces/src/Problem785A.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem785A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count=0;
+        for(int i = 0;i<n;i++){
+            String s = sc.next();
+            if(s.equals("Tetrahedron")) count+=4;
+            else if(s.equals("Cube")) count+=6;
+            else if(s.equals("Octahedron")) count+=8;
+            else if(s.equals("Dodecahedron")) count+=12;
+            else count+=20;
+        }
+        System.out.println(count);
+
+    }
+}
