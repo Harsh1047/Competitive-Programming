4999d27bc8d351121be8558165d2b68cb56284e4
Harsh1047 <jainharsh1047@gmail.com>
2026-03-27 08:55:15 +0530

    Solved: ProblemMIXINGLIQ

---
+++ PracticeCodeChef/src/ProblemMIXINGLIQ.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ProblemMIXINGLIQ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(3* Math.min(a,b/2));
+
+        }
+    }
+}
