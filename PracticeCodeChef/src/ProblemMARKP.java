4289b98a991d0e83d835b2ef7d7037921c3ce6a0
Harsh1047 <jainharsh1047@gmail.com>
2026-03-27 09:05:39 +0530

    Solved: ProblemMARKP

---
+++ PracticeCodeChef/src/ProblemMARKP.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ProblemMARKP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+
+
+        }
+    }
+}
