58cf1643be2561086824835fe9a6a687c59051da
Harsh1047 <jainharsh1047@gmail.com>
2026-03-28 17:41:52 +0530

    Solved: Problem959A

---
+++ PracticeCodeForces/src/Problem959A.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Problem959A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==1) System.out.println("Ehab");
+        else System.out.println("Mahmoud");
+    }
+}
