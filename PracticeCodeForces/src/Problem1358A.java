7a761defa881cc67949bd2ab04c0de68df299a36
Harsh1047 <jainharsh1047@gmail.com>
2026-03-30 17:55:12 +0530

    Solved: Problem1358A

---
+++ PracticeCodeForces/src/Problem1358A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem1358A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            if((n*m)%2==0) System.out.println(n*m/2);
+            else System.out.println(n*m/2+1);
+        }
+    }
+}
