37fdfca13257d3b3472334b856155c13f970fe74
Harsh1047 <jainharsh1047@gmail.com>
2026-03-31 18:50:04 +0530

    Solved: ProblemTWOROLL

---
+++ PracticeCodeChef/src/ProblemTWOROLL.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ProblemTWOROLL {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int k = 50-x;
+            if(k<=2*(6+y-1) && k>=2*y) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
