361bde1a7c5acd8ff5a465c9f30d2c9d2d35566a
Harsh1047 <jainharsh1047@gmail.com>
2026-03-31 18:47:15 +0530

    Solved: ProblemSKONG

---
+++ PracticeCodeChef/src/ProblemSKONG.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ProblemSKONG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int h = sc.nextInt();
+        System.out.println((x-4)*24 + h);
+    }
+}
