82573ba128c7dbeb10d81384a94b26e8ad888cee
Harsh1047 <jainharsh1047@gmail.com>
2026-06-09 20:22:58 +0530

    Solved: Problem1714A

---
+++ PracticeCodeForces/src/Problem1714A.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem1714A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int h = sc.nextInt();
+            int m = sc.nextInt();
+            int[] hour = new int[n];
+            int[] min = new int[n];
+            for(int i=0;i<n;i++) {
+                hour[i] = sc.nextInt();
+                min[i] = sc.nextInt();
+            }
+        }
+    }
+}
